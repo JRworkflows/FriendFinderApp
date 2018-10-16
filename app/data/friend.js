4cda1621e957545782c2b8af787212c7cf12decf
JR <j.romo15@outlook.com>
2018-10-15 23:18:43 -0700

    finished most requirements for page, need final functions and html

---
+++ app/data/friend.js
@@ -0,0 +1,19 @@
+var friends = [
+    {
+        name: "JJ",
+        photo: "JJ@example.com",
+        scores: [
+            5,
+            4,
+            4,
+            1,
+            2,
+            3,
+            1,
+            5,
+            4,
+            3
+        ]
+
+    }
+];
